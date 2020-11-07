69aa7031ff2324ad731f7a7f4f1479193d114c64
Matheus Westhelle <matheus.westhelle@gmail.com>
2020-11-07 16:14:32 -0300

    Implement cost function and forward pass; add class documentation; minor refactoring

--- artificial_neural_network/mlp.py
+++ artificial_neural_network/mlp.py
@@ -5,79 +5,115 @@ from typing import Iterable, List
 import numpy as np
 
 from base_model import BaseModel
+from sigmoid import sigmoid
+
+
+class UninitializedNetworkError(Exception):
+    pass
+
+
+class IncompatibleNetworkStructureError(Exception):
+    pass
 
 
 class MLP(BaseModel):
-    def __init__(self, layers: List[int] = [], regularization: float = 0):
-        """
-        This is a basic implementation of a MultiLayer Perceptron (MLP).
+    """
+    This is a basic implementation of a MultiLayer Perceptron (MLP).
 
+    Attributes
+    ----------
+    weights : dict[int, numpy_array]
+        a dictionary where each key represents a layer and its value contains its
+        respective weights
+
+    layers : list[int]
+        number of layers, represented by integers that tell how many
+        neurons the layer has. The first layer is always the input layer, whilst
+        the last layer is the output layer; all layers in-between are hidden layers
+
+    lambda_ : float
+        regularization factor; must be a non-negative float
+    """
+
+    def __init__(self, layers: List[int] = [], lambda_: float = 0):
+        """
         Parameters
         ----------
-        *layers : tuple of ints
-            Variable number of layers, represented by integers that tell how many
+        layers : list[int]
+            number of layers, represented by integers that tell how many
             neurons the layer has. The first layer is always the input layer, whilst
             the last layer is the output layer; all layers in-between are hidden layers
 
-        regularization : int
-            Regularization factor; must be a non-negative integer
+        lambda_ : float
+            regularization factor; must be a non-negative float
 
-        Attributes
-        ----------
-        weights : dict int -> numpy array
-            A dictionary where each key represents a layer and its value contains its
-            respective weights
         """
-        self.__layers = layers
-        self.__regularization = regularization
+        self._layers = layers
+        self._lambda = lambda_
+        self._weights = dict()
+        layer2size = {idx: size for idx, size in enumerate(self.layers)}
+        for idx in range(len(self.layers) - 1):
+            rows = layer2size[idx + 1]
+            # Add bias column
+            cols = layer2size[idx] + 1
+            self._weights[idx] = np.random.normal(size=(rows, cols))
 
     @property
     def layers(self):
-        return self.__layers
+        return self._layers
 
     @layers.setter
     def layers(self, values):
         for val in values:
-            if val <= 0:
+            if val >= 0:
+                self._layers = values
+            else:
                 raise AttributeError("layers must have a positive number of neurons")
-        self.__layers = values
 
     @property
-    def regularization(self):
-        return self.__regularization
+    def lambda_(self):
+        return self._lambda
 
-    @regularization.setter
-    def regularization(self, value):
+    @lambda_.setter
+    def lambda_(self, value):
         if value >= 0:
-            self.__regularization = value
+            self._lambda = value
         else:
             raise AttributeError("regularization must be a non-negative value")
 
-    def initialize_weights(self):
-        if not hasattr(self, "_weights"):
-            self.weights = dict()
-            layer2size = {idx: size for idx, size in enumerate(self.layers, 1)}
-            for idx in range(len(self.layers) - 1):
-                rows = layer2size[idx + 1]
-                # Add bias column
-                cols = layer2size[idx] + 1
-                self.weights[idx] = np.random.normal(size=(rows, cols))
-        else:
-            raise AttributeError("weights are already initialized")
+    @property
+    def weights(self):
+        return self._weights
+
+    @weights.setter
+    def weights(self, value):
+        self._weights = value
 
     def load_weights(self, weights_filename: str):
         weights_file = Path(weights_filename).resolve(strict=True)
-        layer2size = {idx: size for idx, size in enumerate(self.layers, 1)}
+        layer2size = {idx: size for idx, size in enumerate(self.layers)}
+        if len(layer2size) == 0:
+            raise UninitializedNetworkError(
+                "cannot load weights for an uninitialized "
+                "network; define the network's layers "
+                "before loading weights"
+            )
         self.weights = dict()
         with weights_file.open() as f:
-            for idx, line in enumerate(f, 1):
+            for idx, line in enumerate(f):
                 rows = layer2size[idx + 1]
                 # Add bias column
                 cols = layer2size[idx] + 1
                 layer_weights_text = re.split("[,;]", line.strip())
                 layer_weights = np.array([float(val) for val in layer_weights_text])
-                layer_weights = layer_weights.reshape((rows, cols))
-                self.weights[idx] = layer_weights
+                try:
+                    layer_weights = layer_weights.reshape((rows, cols))
+                except ValueError as err:
+                    raise IncompatibleNetworkStructureError(
+                        f"weights are incompatible with network structure; {err}"
+                    )
+                else:
+                    self.weights[idx] = layer_weights
 
     def load_network_definition(self, network_def_filename: str):
         network_def_file = Path(network_def_filename).resolve(strict=True)
@@ -90,8 +126,18 @@ class MLP(BaseModel):
                 layers.append(layer)
             self.layers = layers
 
-    def forward_pass(self):
-        pass
+    def forward_pass(self, X):
+        a = dict()
+        z = dict()
+        a[0] = np.ones((X.shape[0], X.shape[1] + 1))
+        a[0][:, 1:] = X
+        for k in range(1, len(self.layers) - 1):
+            z[k] = a[k - 1] @ self.weights[k - 1].T
+            a[k] = np.ones((z[k].shape[0], z[k].shape[1] + 1))
+            a[k] = sigmoid(a[k])
+        last_layer = len(self.layers) - 1
+        z[last_layer] = a[last_layer - 1] @ self.weights[last_layer - 1].T
+        return sigmoid(z[last_layer])
 
     def backpropagation(self):
         pass
@@ -102,5 +148,13 @@ class MLP(BaseModel):
     def predict(self, test_data: Iterable[List[str]]):
         pass
 
-    def cost_function(self):
-        pass
+    def cost_function(self, y_pred, y):
+        m = len(y)
+        regularization_term = 0
+        for theta in self.weights:
+            regularization_term += theta[:, 1:] ** 2
+        cost = -(1 / m) * ((y.T @ np.log(y_pred)) + ((1 - y.T) @ np.log(1 - y_pred)))
+        +(self.lambda_ / (2 * m) * regularization_term)
+        return cost
+
+if __name__ == "__main__":

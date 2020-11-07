1e622cba65dbb1daa2eca38abce138575cdb8204
José Mário Reisswitz <jmreisswitz@gmail.com>
2020-11-07 20:02:30 -0300

    add untested implementation

--- artificial_neural_network/normalization.py
+++ artificial_neural_network/normalization.py
@@ -1,5 +1,45 @@
+import pandas as pd
 import numpy as np
 
 
-def normalize_dataset():
-    pass
+def normalize_dataset(dataset: pd.Dataframe):
+    return DatasetNormalizer(dataset).normalize()
+
+
+class DatasetNormalizer:
+    MAX_COLS_FOR_CATEGORICAL_DATA = 10
+
+    def __init__(self, dataset: pd.Dataframe):
+        self.dataset = dataset
+
+    def check_for_numerical_categorical_cols(self):
+        for column in self.dataset.columns:
+            if self.is_numerical_categorical_data(column) \
+                    and self.is_numerical_data(column):
+                self.dataset[column] = self.dataset[column].apply(str)
+
+    def is_numerical_categorical_data(self, column):
+        return len(self.dataset[column].unique()) < self.MAX_COLS_FOR_CATEGORICAL_DATA
+
+    def is_categorical_data(self, column):
+        return not self.is_numerical_data(column)
+
+    def normalize(self):
+        self.check_for_numerical_categorical_cols()
+        self.check_for_categorical_cols()
+        return self.dataset
+
+    def check_for_categorical_cols(self):
+        for column in self.dataset.columns:
+            if self.is_categorical_data(self.dataset[column]):
+                self.separate_column(column)
+
+    def is_numerical_data(self, column):
+        return self.dataset[column].dtype == np.float64 \
+            or self.dataset[column].dtype == np.int64
+
+    def separate_column(self, column):
+        for distinct_value in self.dataset[column].unique():
+            self.dataset[f'{column}_{distinct_value}'] = \
+                self.dataset[column].apply(lambda x: 0 if x != distinct_value else 1)
+

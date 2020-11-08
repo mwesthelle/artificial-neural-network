455a1f69d76ed3ef413fb2c5344142da2b998b6d
Matheus Westhelle <matheus.westhelle@gmail.com>
2020-11-08 16:43:48 -0300

    Formatting

--- artificial_neural_network/normalization.py
+++ artificial_neural_network/normalization.py
@@ -24,8 +24,7 @@ class DatasetNormalizer:
 
     def check_for_numerical_categorical_cols(self):
         for column in self.dataset.columns:
-            if self.has_few_possible_values(column) \
-                    and self.is_numerical_data(column):
+            if self.has_few_possible_values(column) and self.is_numerical_data(column):
                 self.transform_numerical_to_categorical(column)
 
     def normalize_numerical_cols(self):
@@ -36,18 +35,20 @@ class DatasetNormalizer:
     def normalize_categorical_cols(self):
         for column in self.dataset.columns:
             if self.is_categorical_data(column):
-                self.crate_one_hot_representation_for_column(column)
+                self.create_one_hot_representation_for_column(column)
 
     def normalize_numerical_column(self, column):
         max_ = max(self.dataset[column])
         min_ = min(self.dataset[column])
-        self.dataset[column] = self.dataset[column]\
-            .apply(lambda x: (x - min_) / (max_ - min_))
+        self.dataset[column] = self.dataset[column].apply(
+            lambda x: (x - min_) / (max_ - min_)
+        )
 
-    def crate_one_hot_representation_for_column(self, column):
+    def create_one_hot_representation_for_column(self, column):
         for distinct_value in self.dataset[column].unique():
-            self.dataset[f'{column}_{distinct_value}'] = \
-                self.dataset[column].apply(lambda x: 0 if x != distinct_value else 1)
+            self.dataset[f"{column}_{distinct_value}"] = self.dataset[column].apply(
+                lambda x: 0 if x != distinct_value else 1
+            )
 
     def has_few_possible_values(self, column):
         return len(self.dataset[column].unique()) < self.MAX_COLS_FOR_CATEGORICAL_DATA
@@ -61,4 +62,3 @@ class DatasetNormalizer:
     def transform_numerical_to_categorical(self, column):
         self.dataset[column] = self.dataset[column].apply(str)
         self.dataset[column] = self.dataset[column].astype(str)
-

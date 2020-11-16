6dc1ebff520b2ff5ed261545aab05c01ba74ca50
Matheus Westhelle <matheus.westhelle@gmail.com>
2020-11-16 19:18:04 -0300

    Create a one hot encoder class

---
+++ artificial_neural_network/one_hot_encoder.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+class OneHotEncoder:
+    def __init__(self):
+        self.int2label = dict()
+        self.label2int = dict()
+        self.one_hot_encoding = dict()
+
+    def encode(self, labels):
+        self.int2label = {idx: label for idx, label in enumerate(labels)}
+        label2int = {label: idx for idx, label in enumerate(labels)}
+        for label in labels:
+            self.one_hot_encoding[label] = np.zeros(len(labels))
+            self.one_hot_encoding[label][label2int[label]] = 1
+
+    def decode(self, one_hot_array):
+        return self.int2label[one_hot_array.index(1)]
